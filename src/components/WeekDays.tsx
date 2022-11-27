72a68fa85e07f40d2a20bbe286f687555ca796d8
Vincent Limo <48591371+v-limo@users.noreply.github.com>
2022-11-27 20:50:54 +0200

    update Week days to remove cursor pointer

--- src/components/WeekDays.tsx
+++ src/components/WeekDays.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react"
 
 const WeekDays = () => {
   return (
     <div>
       <div className=" flex flex-col items-center justify-between">
-        <p className="cursor-pointer  p-3  text-sm">Mon</p>
-        <p className="cursor-pointer  p-3  text-sm">Wen</p>
-        <p className="cursor-pointer  p-3  text-sm">Fri</p>
+        <p className="  p-3  text-sm">Mon</p>
+        <p className="  p-3  text-sm">Wen</p>
+        <p className="  p-3  text-sm">Fri</p>
       </div>
     </div>
   )
