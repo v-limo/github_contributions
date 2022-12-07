eedf429c11a04f312a7f4e16689ba0063d56bbc5
Vincent Limo <48591371+v-limo@users.noreply.github.com>
2022-12-07 19:04:50 +0200

    Update YearComponent.tsx

--- src/components/YearComponent.tsx
+++ src/components/YearComponent.tsx
@@ -15,7 +15,7 @@ export const YearComponent = ({ handleClick, year }: Props) => {
             key={i + item}
             value={new Date().getFullYear() + i}
             onClick={(e) => handleClick(e)}
-            className={`
+            className={`no-underline
             cursor-pointer  p-3  text-sm text-gray-500 hover:text-gray-700
             ${
               year === new Date().getFullYear() + i
