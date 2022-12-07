0b744a174ddd5e0209939bfbd6f740069bb91b12
Vincent Limo <48591371+v-limo@users.noreply.github.com>
2022-12-07 19:13:54 +0200

    Update CalendarHeader.tsx

--- src/components/CalendarHeader.tsx
+++ src/components/CalendarHeader.tsx
@@ -8,7 +8,7 @@ type Props = {
 const ContributionCalendar = ({ name, contributions }: Props) => {
   return (
     <div>
-      <h1 className="m-auto  text-3xl font-bold text-gray-800 ">{name}</h1>
+      <h1 className="mx-auto my-3  text-3xl font-bold text-gray-800 ">{name}</h1>
       <div className="flex items-center justify-between ">
         <p className="mb-2 text-sm  text-gray-800">
           {contributions.toLocaleString()} contributions in the last year
